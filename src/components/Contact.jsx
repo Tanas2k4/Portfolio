bc3794f402a9094cd54730c71645aed6cdd88fac
Tanas2k4 <bongbo355@gmail.com>
2025-09-26 21:30:58 +0700

    add Contact and mail to

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  return <div id="#contact" className="flex-min-h-[70px] min-w-full items-center justify-center">
+    <div className="flex flex-col items-center justify-center gap-3 space-y-6 p-14">
+      <h1 className="text-center text-5xl md:text-7xl">
+        <span className="bg-gradient-to-r from-indigo-500 to-blue-500 bg-clip-text text-transparent">
+          Get in touch
+        </span>
+      </h1>
 
-export default Contact
+      <p className="text-center text-lg font-semibold text-gray-500">
+        Want to chat? sent me an E-mail through this button and i'll  respond
+        Whenever I can 
+      </p>
+
+      <a href="mailto:t.ttan25204@gmail.com" className="text-nowrap rounded-lg  border border-indigo-600 bg-black px-5 
+      py-3 text-lg font-bold text-white shadow-lg shadow-indigo-700 transition-all duration-300
+      hover:-translate-y-2 hover:shadow-xl hover:shadow-indigo-600">
+        Contact Me
+      </a>
+    </div>
+  </div>;
+};
+
+export default Contact;
